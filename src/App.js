4fddfce4002eaec83b7688e8cde6dec6411f8e21
Dims2407 <108788284+Dims2407@users.noreply.github.com>
2022-11-25 16:10:48 +0300

    до 1.12

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import React, {useState} from "react";
 import './styles/App.css'
-import PostItem from "./Components/PostItem";
 import PostList from "./Components/PostList";
+import PostForm from "./Components/PostForm";
+import MySelect from "./Components/UI/MySelect";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Description'},
-        {id: 2, title: 'Javascript 2', body: 'Description 2'},
-        {id: 3, title: 'Javascript 3', body: 'Description 3'}
+        {id: 1, title: 'сс', body: 'мм'},
+        {id: 2, title: 'ааа 2', body: 'оо 2'},
+        {id: 3, title: 'нн 3', body: 'дд 3'}
     ])
-    const [posts2, setPosts2] = useState([
-        {id: 1, title: 'python', body: 'Description'},
-        {id: 2, title: 'python 2', body: 'Description 2'},
-        {id: 3, title: 'python 3', body: 'Description 3'}
-    ])
+
+    const [selectedSort, setSelectedSort] = useState('')
+    const createPost = (NewPost) => {
+        setPosts([...posts, NewPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+const sortPost = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+}
+
     return (
         <div className="App">
-<PostList posts={posts} title='Список постов про JS'/>
-            <PostList posts={posts2} title='Список постов про PTN'/>
+            <PostForm create={createPost}/>
+            <hr style={{margin: "15px 0"}}/>
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPost}
+
+                    defaultValue='Сортировка:'
+                    options={[
+                        {value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'}
+                    ]}/>
+            </div>
+            {posts.length
+                ? <PostList remove={removePost} posts={posts} title='Список постов про JS'/>
+                : <h1 style={{textAlign: "center"}}>Посты не найдены!</h1>
+            }
+
+
         </div>
     );
 }
---
+++ src/Components/PostForm.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import MyInput from "./UI/Input/MyInput";
+import MyButton from "./UI/Button/MyButton";
+
+const PostForm = ({create}) => {
+    const [post, setPost] = useState({title: '', body: ''})
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        const NewPost = {...post, id: Date.now()}
+        create(NewPost)
+        setPost({title: '', body: ''})
+    }
+
+    return (
+        <form>
+                <MyInput value={post.title} onChange={e => setPost({...post, title: e.target.value})} type='text'
+                         placeholder='Название поста'/>
+                <MyInput value={post.body} onChange={e => setPost({...post, body: e.target.value})} type='text' placeholder='Описание поста'/>
+                <MyButton onClick={addNewPost}>Добавить пост</MyButton>
+            </form>
+
+    );
+};
+
+export default PostForm;
