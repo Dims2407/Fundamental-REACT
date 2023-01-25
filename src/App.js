a8dace29f2c2ac65857c854c84e68fef22dd03dd
Dims2407 <108788284+Dims2407@users.noreply.github.com>
2023-01-25 15:25:06 +0300

    до 2.22

--- src/App.js
+++ src/App.js
@@ -1,65 +1,19 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import './styles/App.css'
-import PostList from "./Components/PostList";
-import PostForm from "./Components/PostForm";
-import PostFilter from "./Components/PostFilter";
-import MyModal from "./Components/UI/MyModal/MyModal";
-import MyButton from "./Components/UI/Button/MyButton";
-import {usePosts} from "./hooks/UsePosts";
-import PostService from "./API/PostServise";
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./Components/UI/Navbar/Navbar";
+import AppRouter from "./Components/UI/AppRouter";
+
 
 function App() {
-    const [posts, setPosts] = useState([])
-
-    const [filter, setFilter] = useState({sort: '', query: ''})
-    const [modal, setModal] = useState(false)
-    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    const [isPostsLoading, setIsPostsLoading] = useState(false)
-
-    useEffect(()=> {
-        fetchPosts()
-    }, [])
-
-    const createPost = (NewPost) => {
-        setPosts([...posts, NewPost])
-        setModal(false)
-    }
-
-    async function fetchPosts() {
-        setIsPostsLoading(true)
-        setTimeout(async ()=> {
-            const posts = await PostService.getAll()
-            setPosts(posts)
-            setIsPostsLoading(false)
-        }, 1000)
-
-    }
-
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
-
     return (
-        <div className="App">
-            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-                Новый пост
-            </MyButton>
-            <MyModal visible={modal} setVisible={setModal}>
-                <PostForm create={createPost}/>
-            </MyModal>
-
-            <hr style={{margin: "15px 0"}}/>
-            <PostFilter filter={filter} setFilter={setFilter}/>
-            {isPostsLoading
-                ? <h1>Идёт загрузка...</h1>
-                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов про JS'/>
-
-            }
+        <BrowserRouter>
+            <Navbar/>
+            <AppRouter/>
+        </BrowserRouter>
+    )
 
 
-
-        </div>
-    );
 }
 
 export default App;
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,80 @@
+import React, {useEffect, useState} from "react";
+import '../styles/App.css'
+import PostList from "../Components/PostList";
+import PostForm from "../Components/PostForm";
+import PostFilter from "../Components/PostFilter";
+import MyModal from "../Components/UI/MyModal/MyModal";
+import MyButton from "../Components/UI/Button/MyButton";
+import {usePosts} from "../hooks/UsePosts";
+import PostService from "../API/PostServise";
+import Loader from "../Components/UI/Loader/Loader";
+import {UseFetching} from "../hooks/UseFetching";
+import {getPageCount} from "../utils/pages";
+import Pagination from "../Components/UI/Pagination/Pagination";
+
+function Posts() {
+
+    const [posts, setPosts] = useState([])
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false)
+
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+
+    const [fetchPosts, isPostsLoading, postError] = UseFetching(async (limit, page) => {
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit))
+    })
+
+
+    useEffect(() => {
+        fetchPosts(limit, page)
+    }, [])
+
+    const createPost = (NewPost) => {
+        setPosts([...posts, NewPost])
+        setModal(false)
+    }
+
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const changePage = (page) => {
+        setPage(page)
+        fetchPosts(limit, page)
+    }
+
+    return (
+        <div className="App">
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Новый пост
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+
+            <hr style={{margin: "15px 0"}}/>
+            <PostFilter filter={filter} setFilter={setFilter}/>
+            {postError &&
+                <h1>Произошла ошибка ${postError}</h1>
+            }
+
+            {isPostsLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '150px'}}><Loader/></div>
+                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов про JS'/>
+
+            }
+            <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
+
+        </div>
+    );
+}
+
+export default Posts;
