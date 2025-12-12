de63e6914ca94ba4fff81aedfe8db60cfd42d81b
Dims2407 <108788284+Dims2407@users.noreply.github.com>
2025-12-12 20:13:06 +0300

    rework sending on tg

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,10 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.js',
+    supportFile: false,
     video: false, // Можно включить при необходимости
-    screenshotOnRunFailure: true,
+    screenshotOnRunFailure: false,
+
   },
   reporter: 'mochawesome',
   reporterOptions: {
