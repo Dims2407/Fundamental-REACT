bf64a84a1d7ccbc9fda55e08072564d4f19c4074
Dims2407 <108788284+Dims2407@users.noreply.github.com>
2025-12-12 16:59:22 +0300

    fix error with supp file

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000', // или другой URL, где запускается ваше приложение
-    supportFile: 'cypress/support/e2e.js',
+    supportFile: false,  // ⬅️ Отключает проверку файла поддержки,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+
   },
 });
